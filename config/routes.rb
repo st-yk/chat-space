c6ce152e761c74544a19c54f1839b84c2e056bd7
st-yk <meisdcw@gmail.com>
2019-12-06 14:47:29 +0900

    groups

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
+
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
