872d8ad90180099c138f4e00bccee81c2e1da720
st-yk <meisdcw@gmail.com>
2019-12-10 12:34:56 +0900

    message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,6 +12,16 @@ $(function(){
       processData: false,
       contentType: false
     })
+    function buildHTML(message){
+      // 「もしメッセージに画像が含まれていたら」という条件式
+      if (message.image) {
+        var html = //メッセージに画像が含まれる場合のHTMLを作る
+      } else {
+        var html = //メッセージに画像が含まれない場合のHTMLを作る
+      }
+      return html
+    }
+  
   });  
 });
 
