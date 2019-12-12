9ed77c8a26b73dbdd5b8d55d2f10cce3235e75ad
st-yk <meisdcw@gmail.com>
2019-12-12 15:15:54 +0900

    自動更新add

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,60 @@
 $(function(){
-  // console.log(last_message_id);
   var buildHTML = function(message) {
     if (message.text && message.image) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.date  +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.text +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="message" data-id= message.id 
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        message.user_name
+                      </div>
+                      <div class="upper-message__date">
+                        message.date 
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        message.text 
+                      </p>
+                      <img src=" + message.image + " class="lower-message__image" >
+                    </div>
+                  </div>`
     } else if (message.text) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.date  +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.text +
-          `</p>` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="message" data-message-id= + message.id + >
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        message.user_name
+                      </div>
+                      <div class="upper-message__date">
+                        message.date
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        message.text
+                      </p>
+                    </div>
+                  </div>`
     } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.date  +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="message" data-message-id= + message.id + >
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        message.user_name 
+                      </div>
+                    <div class="upper-message__date">
+                      message.date  
+                    </div> 
+                  </div>
+                    <div class="lower-message"> 
+                      <img src=" + message.image + " class="lower-message__image" > 
+                    </div> 
+                  </div>
     };
-    return html;
+    return html;`
   };
 
   $("#new_message").on("submit", function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = location.href ;
-    console.log(url)
     $.ajax({
       url: url, 
       type: 'POST',  
@@ -81,31 +76,24 @@ $(function(){
     }) 
   });  
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = message.id
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
 
     })
     .fail(function() {
-      console.log('error');
+      alert("error");
     });
   };
   setInterval(reloadMessages, 7000);
